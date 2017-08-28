18bee6fcb2456d5d4da831b0f617cd844fbb84b0
Chris Cankov <cankov.chris@gmail.com>
2017-08-27 21:59:18 -0700

    Practice using destructuring

--- app.ts
+++ app.ts
@@ -3,10 +3,22 @@ import { Book, Logger, Author, Librarian, Magazine } from './interfaces';
 import { UniversityLibrarian, ReferenceItem } from './classes';
 import * as util from './lib/utilityFunctions';
 
-function PrintBookInfo(item: Book): void {
-  console.log(`${item.title} was authored by ${item.author}`);
+function PrintBookInfo({ title: booktitle, author: bookauthor }: Book): void {
+  console.log(`${booktitle} was authored by ${bookauthor}`);
 }
 
 let [book1, book2] = util.GetAllBooks();
+
+function LogFavoriteBooks([book1, book2, ...others]: Book[]) {
+  PrintBookInfo(book1);
+  PrintBookInfo(book2);
+  console.log(others);
+}
+
+// LogFavoriteBooks(util.GetAllBooks());
+
+// let { title: booktitle, author: bookauthor } = book1;
+// console.log(booktitle);
+// console.log(bookauthor);
+
 PrintBookInfo(book1);
-PrintBookInfo(book2);
--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var util = require("./lib/utilityFunctions");
-function PrintBookInfo(item) {
-    console.log(item.title + " was authored by " + item.author);
+function PrintBookInfo(_a) {
+    var booktitle = _a.title, bookauthor = _a.author;
+    console.log(booktitle + " was authored by " + bookauthor);
 }
 var _a = util.GetAllBooks(), book1 = _a[0], book2 = _a[1];
+function LogFavoriteBooks(_a) {
+    var book1 = _a[0], book2 = _a[1], others = _a.slice(2);
+    PrintBookInfo(book1);
+    PrintBookInfo(book2);
+    console.log(others);
+}
+// LogFavoriteBooks(util.GetAllBooks());
+// let { title: booktitle, author: bookauthor } = book1;
+// console.log(booktitle);
+// console.log(bookauthor);
 PrintBookInfo(book1);
-PrintBookInfo(book2);
 //# sourceMappingURL=app.js.map
